4388fb62c89d06fa1b9615501be8b89c78d30a4c
reuben <creubend@gmail.com>
2022-06-11 16:18:30 -0600

    test: A forcast request is expecting data

---
+++ spec/requests/api/v1/forecast/forecast_request_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper' 
+
+describe 'forecast request' do 
+    it 'will return the forcast for a location', :vcr do 
+        get '/api/v1/forecast?location=denver,co'
+        expect(response).to be_successful
+        parsed = JSON.parse(response.body, symbolize_names: true)[:data]
+        expect(parsed).to have_key(:data)
+    end
+end
