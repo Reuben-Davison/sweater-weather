fed427b8ca9dadaeeb34a496dae1d101210cc3df
reuben <creubend@gmail.com>
2022-06-13 12:11:52 -0600

    finished

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BooksController < ApplicationController 
     def index
         weather = Api::V1::BooksController.get_weather(params[:location])
-        render json: BooksSerializer.new(BooksFacade.make_books_data(params[:location], params[:quantity]), weather)
+        render json: BooksSerializer.new(BooksFacade.make_books_data(params[:location], params[:quantity], weather))
     end
 
 #this method is in the controller so that way books facade didnt have to call on a weather facade
--- app/poros/books.rb
+++ app/poros/books.rb
@@ -5,7 +5,7 @@ class Books
         @destination = location 
         @forecast = {summary: weather[:conditions], temperature: (weather[:temperature].to_s + " F")}
         @total_books_found = books[:numFound]
-        @books = book_info(books)[0..(quantity - 1)]
+        @books = book_info(books)[0..(quantity.to_i - 1)]
     end
 
     def book_info(books)
