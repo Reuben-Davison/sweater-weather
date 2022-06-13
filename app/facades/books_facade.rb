5f920ba2edf556571fe2b9cd88f176a260a286a5
reuben <creubend@gmail.com>
2022-06-13 11:34:21 -0600

    feat: book_info returns values expected for a book

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -1,8 +1,8 @@
 class BooksFacade 
     def self.make_books_data(location, quantity, weather)
-        Books.new(location, get_books(location), weather)
+        Books.new(location, get_books(location), weather, quantity)
     end
-    def get_books(location)
+    def self.get_books(location)
         LibraryService.get_books_details(location)
     end
 end
--- app/poros/books.rb
+++ app/poros/books.rb
@@ -1,8 +1,22 @@
 class Books 
     attr_reader :id, :destination, :forecast, :total_books_found, :books
 
-    def initialize(location, books, weather)
-        binding.pry
-      @location = location 
+    def initialize(location, books, weather, quantity)
+        @destination = location 
+        @forecast = {summary: weather[:conditions], temperature: (weather[:temperature].to_s + " F")}
+        @total_books_found = books[:numFound]
+        @books = book_info(books)[0..(quantity - 1)]
     end
+
+    def book_info(books)
+        hash = books[:docs].map do |book|
+            {
+                :isbn => book[:isbn],
+                :title => book[:title],
+                :publisher => book[:publisher]
+            }
+        end 
+        hash
+    end
+
 end
