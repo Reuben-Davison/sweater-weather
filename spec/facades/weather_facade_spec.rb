a6530483505805b258bad10f58bccc5467cbc14b
reuben <creubend@gmail.com>
2022-06-13 08:51:27 -0600

    test: weather attributes class type are correct

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -6,8 +6,8 @@ describe 'Weather facade' do
         lng = -104.984853
         weather = WeatherFacade.get_weather(lat, lng)
         expect(weather).to be_a(Weather) 
-        expect(weather.daily).to be_a(Hash)
+        expect(weather.daily).to be_a(Array)
         expect(weather.current).to be_a(Hash)
-        expect(weather.hourly).to be_a(Hash)
+        expect(weather.hourly).to be_a(Array)
     end
 end
