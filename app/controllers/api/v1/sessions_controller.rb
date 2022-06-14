6d6f49164aa6501b93a439d239baa9725a6b94b5
reuben <creubend@gmail.com>
2022-06-14 12:12:53 -0600

    test: Sad path for incorrect loging created

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SessionsController < ApplicationController 
     def create 
         user = User.find_by(email: params[:email])
-        binding.pry
         if user.authenticate(params[:password])
             render json: UserSerializer.new(user), status: 201
         else 
