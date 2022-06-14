de0a1073cbc7d661c86dd5709f62c44f9972fc5b
reuben <creubend@gmail.com>
2022-06-14 15:49:22 -0600

    refactor: Rails routes to go to a plural controller

--- app/controllers/api/v1/background_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::BackgroundController < ApplicationController 
-    def location 
-        render json: ImageSerializer.new(BackgroundFacade.get_photo(params[:location]))
-    end
-end
