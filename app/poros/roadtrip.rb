de10b404be102ea7657c785afa00c2192580c467
reuben <creubend@gmail.com>
2022-06-14 21:31:30 -0600

    feat: Roadtrip poro is able to return all expected attributes

--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -5,20 +5,24 @@ class Roadtrip
         @start_city = origin
         @end_city = destination
         @travel_time = find_travel_time(trip_details)
-        binding.pry
         @weather_at_eta = eta_weather(weather)
+        binding.pry
     end
 
     def find_travel_time(data)
-        time = data[:route][:legs][0][:time]
-        secs = time.divmod(60)[1]
-        min = time.divmod(60)[0].divmod(60)[1]
-        hours = time.divmod(60)[0].divmod(60)[0]
+        @time = data[:route][:legs][0][:time]
+        secs = @time.divmod(60)[1]
+        min = @time.divmod(60)[0].divmod(60)[1]
+        hours = @time.divmod(60)[0].divmod(60)[0]
         return "#{hours} hours, #{min} minutes, #{secs} seconds"
     end
 
     def eta_weather(data)
-        
-
+        target_time = Time.now.to_i + @time
+        hour = data[:hourly].find{|hour| hour[:dt] >= target_time}
+        {
+           :temperature => hour[:temp],
+           :conditions => hour[:weather][0][:description]
+        }
     end
 end
